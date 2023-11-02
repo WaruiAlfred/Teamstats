1c0cf54988050255232f132f1110831ef722c396
WaruiAlfred <alfredwarui17@gmail.com>
2023-11-03 01:38:26 +0300

    modify navbar component styles

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,7 +3,7 @@ import { MoreVertical } from "lucide-react";
 const Navbar = () => {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-4 px-2">
+      <div className=" flex flex-wrap items-center justify-between mx-auto py-4 px-4">
         <a href="https://flowbite.com/" className="flex items-center">
           <span className="self-center uppercase text-2xl font-semibold whitespace-nowrap dark:text-gray-200">
             TEAMSTATS
