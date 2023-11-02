5db1c193c46e6ff98b1fe279ea6b84aec7e96661
WaruiAlfred <alfredwarui17@gmail.com>
2023-11-03 01:37:26 +0300

    wrap App in queryclientprovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import PlayerStats from "./components/PlayerStats";
 import Navbar from "./components/layout/Navbar";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Navbar />
-    </>
+      <PlayerStats />
+    </QueryClientProvider>
   );
 }
 
